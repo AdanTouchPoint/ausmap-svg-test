9fd65b3087c0eebdef97d950f7248ff3cf66c720
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-12-28 14:53:08 -0600

    fristCommit

---
+++ src/app/components/AusMap.js
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import "../css/map.css";
+import WA from "./WA";
+import VIC from "./VIC";
+import TAS from "./TAS";
+import SA from "./SA";
+import QLD from "./QLD";
+import NSW from "./NSW";
+import NT from "./NT";
+
+const AusMap = () => {
+  return (
+    <div >
+      <svg version="1.2" viewBox="0 0 1000 1222" xmlns="http://www.w3.org/2000/svg" >
+          <NT />
+          <NSW />
+          <WA />
+          <VIC />
+          <TAS />
+          <SA />
+          <QLD />
+    
+      </svg>
+    </div>
+  );
+};
+
+export default AusMap;
+
